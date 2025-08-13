2e736c09a5c825dc0bfbc1bde003efdc67cc4ffd
sarvar523 <sarvarakmalovich523@gmail.com>
2025-08-13 11:31:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let isBlue = false; // dastlabki holat
+
+let myFunction = function () {
+    if (!isBlue) {
+         alert('Xayr')
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        isBlue = true;
+       rue
+    } else {
+        alert('Qalesan');
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        isBlue = false;
+      
+    }
+}
